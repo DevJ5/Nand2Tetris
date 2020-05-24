a41504656c3f4af8605ceebaf3b4cde7f4f55227
DevJ5 <idevelopjs@gmail.com>
2020-05-24 13:57:18 +0200

    Computer

--- 04/fill/foo.asm
+++ 04/fill/foo.asm
@@ -1,21 +1,3 @@
-    @i
-    M = 0
-    @3
-    D = A
-    @n
-    M = D
-(LOOP)
-    @n
-    D = M
-    @i
-    M = M + 1
-    D = D - M // This has to happen, cant do a compare with M underneath the Jump to address.
-    @END
-    D; JEQ
-
-    @LOOP
-    0; JMP
-
-(END)
-    @END
-    0;JMP
+@2
+A=M
+D=M
